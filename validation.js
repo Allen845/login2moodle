ee1210ff02c872571c4c82ee35cd599e3763c48b
Fernando Perez <103344448+Allen845@users.noreply.github.com>
2024-08-13 18:49:05 -0500

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('form')
+const firstname_input = document.getElementById('firstname-input')
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat-password-input')
+const error_message = document.getElementById('error-message')
+
+form.addEventListener('submit', (e) => {
+  let errors = []
+
+  if(firstname_input){
+    // If we have a firstname input then we are in the signup
+    errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value)
+  }
+  else{
+    // If we don't have a firstname input then we are in the login
+    errors = getLoginFormErrors(email_input.value, password_input.value)
+  }
+
+  if(errors.length > 0){
+    // If there are any errors
+    e.preventDefault()
+    error_message.innerText  = errors.join(". ")
+  }
+})
+
+function getLoginFormErrors(email, password){
+  let errors = []
+
+  if(email === '' || email == null){
+    errors.push('Usuario es Requerido')
+    email_input.parentElement.classList.add('incorrect')
+  }
+  if(password === '' || password == null){
+    errors.push('Contraseña es Requerida')
+    password_input.parentElement.classList.add('incorrect')
+  }
+
+  return errors;
+}
+
+const allInputs = [firstname_input, email_input, password_input, repeat_password_input].filter(input => input != null)
+
+allInputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if(input.parentElement.classList.contains('incorrect')){
+      input.parentElement.classList.remove('incorrect')
+      error_message.innerText = ''
+    }
+  })
+})
